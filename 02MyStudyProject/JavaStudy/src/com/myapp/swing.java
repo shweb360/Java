188e1bbef84797c24e774f626f8d1bd4f6174f37
wush <1069680815@qq.com>
2018-11-29 11:12:38 +0800

    Swing包括了图形用户界面

---
+++ 02MyStudyProject/JavaStudy/src/com/myapp/swing.java
@@ -0,0 +1,38 @@
+package com.myapp;
+import javax.swing.*;
+
+public class swing {
+
+	public static void main(String[] args) {		
+		//显示应用GUI		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGui();
+			}
+		});
+
+	}
+	
+	private static void createAndShowGui()
+	{
+		//确定一个外观；
+		JFrame.setDefaultLookAndFeelDecorated(true);
+		
+		// 创建及设置窗口
+		JFrame frame=new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 // 添加 "Hello World" 标签
+		JLabel lable=new JLabel("Hello Swing");
+		
+		frame.getContentPane().add(lable);
+		
+		//显示窗口
+		
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+			
+	}
+
+}
