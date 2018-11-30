62904685fccc8821488c0f1b61228e78da97e608
wush <1069680815@qq.com>
2018-11-30 17:36:14 +0800

    进程同步机制

---
+++ 02MyStudyProject/JavaStudy/src/com/myapp/ThreadSafe.java
@@ -0,0 +1,38 @@
+package com.myapp;
+
+public class ThreadSafe implements Runnable {
+	int num = 10;
+	public void run() {
+		while (true) {
+		   synchronized ("") {		
+		
+			if (num > 0) {
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					
+					e.printStackTrace();
+				}
+				System.out.println("ticks：" + num--);
+			}
+		   }
+		}
+	}
+
+	public static void main(String[] args) {
+		//实例化对象
+		ThreadSafe t = new ThreadSafe();
+		//以该类对象，实例化四个线程
+		Thread ta = new Thread(t);
+		Thread tb = new Thread(t);
+		Thread tc = new Thread(t);
+		Thread td = new Thread(t);
+		//分别启动线程
+		ta.start();
+		tb.start();
+		tc.start();
+		td.start();
+
+	}
+
+}
