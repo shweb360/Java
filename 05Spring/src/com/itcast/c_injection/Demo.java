b6d7d90ccba03f074897043f4b1467c65253ddcb
wush <1069680815@qq.com>
2019-02-01 13:57:03 +0800

    复杂类型注入：数组、list、map和properties

--- 05Spring/src/com/itcast/c_injection/Demo.java
+++ 05Spring/src/com/itcast/c_injection/Demo.java
@@ -12,7 +12,7 @@ public class Demo {
 	
 	
 	@Test
-	public void fun() {
+	public void fun1() {
 		
 	@SuppressWarnings("resource")
 	//创建容器
@@ -24,5 +24,22 @@ public class Demo {
 	//
 	User user=(User) appContext.getBean("user2");
 	System.out.println(user);
+	
+	CollectionBean cb= (CollectionBean) appContext.getBean("cb");
+	System.out.println(cb);
+	
 	}
+	@Test
+	public void fun2() {
+		
+	@SuppressWarnings("resource")
+	//创建容器
+	ApplicationContext appContext=new ClassPathXmlApplicationContext("com/itcast/c_injection/applicationContext.xml");
+	
+	
+	CollectionBean cb= (CollectionBean) appContext.getBean("cb");
+	System.out.println(cb);
+	
+	}
+	
 }
